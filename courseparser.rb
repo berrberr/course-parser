7182048659e60a519ee8444f228906689de2a25a
Alex Gabriel <alex@aegabriel.com>
2013-12-09 22:30:35 -0500

    course titles,desc

--- courseparser.rb
+++ courseparser.rb
@@ -8,7 +8,7 @@ require 'open-uri'
 # antireq: span class='course-prereq'
 
 def is_course_code(course_code)
-  return !!(course_code =~ /^[0-9][A-Z][A-Z][0-9]/i)
+  return !!(course_code =~ /^[0-9][A-Z][A-Z0-9][0-9]/i)
 end
 
 doc = Nokogiri::HTML(open('http://registrar.mcmaster.ca/CALENDAR/current/pg2486.html'))
@@ -18,34 +18,22 @@ doc.css('div.item-container').each do |coursebox|
   row = {}
   coursebox.css('span.course-code').each do |coursecode|
     if is_course_code(coursecode.content.strip)
-      row[:coursecode] = coursecode.content.strip
+      row[:code] = coursecode.content.strip
     end
   end
-  output << row
+  if coursebox.css('span.course-title')[0] then 
+    row[:title] = coursebox.css('span.course-title')[0].content.strip 
+  end
+  if coursebox.css('span.course-desc')[0] then 
+    row[:description] = coursebox.css('span.course-desc')[0].content.strip 
+  end
+  if coursebox.css('span.course-prereq')[1] then
+    row[:prereq] = coursebox.css('span.course-prereq')[1]["id"]
+  end
+  
+  if !!row then
+    output << row
+  end
 end
-
-puts output
-# doc.css('span.course-code').each do |coursecode|
-#   if is_course_code(coursecode.content.strip)
-#     output << {:code => coursecode.content}
-#   end
-# end
-# puts output.inspect
-
-# i = 0
-# doc.css('span.course-title').each do |title|
-#   if !!output[i]
-#     output[i].merge!({:title => title.content})
-#   end
-#   i = i + 1
-# end
-
-# i = 0
-# doc.css('span.course-desc').each do |desc|
-#   if !!output[i]
-#     output[i].merge!({:desc => desc.content})
-#   end
-#   i = i + 1
-# end
 
 puts output
