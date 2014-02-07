cc85ae2ebd45498f70d107287e093d8b0be67d8f
Alex Gabriel <alex@aegabriel.com>
2014-02-06 23:18:21 -0500

    Added some styling. Added courselist model.

--- web/public_webapp/scripts/models/courselist.js
+++ web/public_webapp/scripts/models/courselist.js
@@ -1,6 +1,18 @@
 define(['models/course'], function(Course) {
   var CourseList = Backbone.Collection.extend({
-    model: Course
+    model: Course,
+    url: 'http://api.courses.dev/course',
+
+    initialize: function() {
+      var self = this;
+      this.ready = false;
+      this.fetch()
+        .done(function(collection) {
+          console.log('FETCH: ', collection);
+          self.collection = collection;
+          self.ready = true;
+        });
+    }
   });
   
   return CourseList;
--- web/public_webapp/scripts/views/course/subjectitemview.js
+++ web/public_webapp/scripts/views/course/subjectitemview.js
@@ -3,10 +3,11 @@ define(['text!templates/course/subjectitem.html', 'models/course', 'views/course
     template: _.template(template),
 
     events: {
-      'click': 'showSubjectCourses'
+      'click': 'toggleSubjectCourses'
     },
 
     initialize: function() {
+      this.visible = false;
     },
 
     render: function() {
@@ -17,19 +18,25 @@ define(['text!templates/course/subjectitem.html', 'models/course', 'views/course
 
     //On clicking this treenode show the subnode (the course list)
     //Add a sibling div containing a ul with each li being a courseitemview
-    showSubjectCourses: function() {
+    toggleSubjectCourses: function() {
       //console.log(this.model);
       event.preventDefault();
       var $el = $(this.el);
-      $el.after('<div id="course-leaf" class="list-group"></div>');
-      $el = $('#course-leaf');
-      this.model.fetch()
-        .done(function(collection, response) {
-          _.each(collection, function(course) {
-            var item = new CourseItemView({ model: new Course(course) });
-            $el.append(item.render().el);
+      $('.subtree-row').remove();
+      if(this.visible) {
+        this.visible = false;
+      } else {
+        $el.after('<div class="row subtree-row" id="subtree-row"><div class="col-sm-11 subtree"><div id="course-leaf"></div></div></div>');
+        $el = $('#course-leaf');
+        this.model.fetch()
+          .done(function(collection, response) {
+            _.each(collection, function(course) {
+              var item = new CourseItemView({ model: new Course(course) });
+              $el.append(item.render().el);
+            });
           });
-        });
+        this.visible = true;
+      }
     }
   })
 
