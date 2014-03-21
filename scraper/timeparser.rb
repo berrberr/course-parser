4ab6307d6750227d8c026db5ccc809605b39ad8c
Alex Gabriel <alex@aegabriel.com>
2014-03-20 23:04:23 -0400

    For additions to time parser. Now downloads the course TT page.

--- scraper/timeparser.rb
+++ scraper/timeparser.rb
@@ -2,53 +2,66 @@ require 'mechanize'
 require 'nokogiri'
 require 'fileutils'
 
-# create Mechanize instance
-a = Mechanize.new
-a.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+#URL for timetable page
+MTTURL = "https://adweb.cis.mcmaster.ca/mtt/"
 
-# get the subject codes from the tt page
-ttPage = a.get("https://adweb.cis.mcmaster.ca/mtt/")
-ttForm = ttPage.form('MTTSearch')
 
-subjects = []
-courses = ['1AA3', '2AA3']
-doc = Nokogiri::HTML(ttPage.body)
-doc.css('select[name=subject]').css('option').each do |subject|
-  if(subject['value'] != 'all') then subjects << subject['value'] end
-end
+def getTimetablePages()
 
-i = 0
-if(!subjects.empty?) then
-  if(i < 6) then
-    i = i + 1
-    puts i
+  #create Mechanize instance
+  a = Mechanize.new
+  a.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  #get the subject codes from the tt page
+  ttPage = a.get(MTTURL)
+
+  subjects = []
+  courses = ['2AA3', '1AA3']
+  doc = Nokogiri::HTML(ttPage.body)
+  doc.css('select[name=subject]').css('option').each do |subject|
+    if(subject['value'] != 'all') then subjects << subject['value'] end
+  end
+
+  i = 0
+  if(!subjects.empty?) then
+    #each subject code
     subjects.each do |subject_code|
-      courses.each do |course_code|
-        ttForm.course = course_code
-        ttForm.subject = subject_code
-        resp = a.submit(ttForm, ttForm.buttons.first)
+      if(i < 3) then
+        i = i + 1
+        #each course for that subject
+        courses.each do |course_code|
 
-        tt_doc = Nokogiri::HTML(resp.body)
-        if(!doc.css('a[target=details]').empty?) then
-          courseHref = doc.css('a[target=details]')[0]['href']
-          puts courseHref
-          # coursePage = a.get('https://adweb.cis.mcmaster.ca/mtt/')
+          #get the TT main page, get the form from it, submit contents
+          ttPage = a.get(MTTURL)
+          ttForm = ttPage.form('MTTSearch')
+          ttForm.course = course_code
+          ttForm.subject = subject_code
+          resp = a.submit(ttForm, ttForm.buttons.first)
+          tt_doc = Nokogiri::HTML(resp.body)
 
-          # fPath = 'courses/' + subject_code + '/' + course_code + '.html'
-          # dirname = File.dirname(fPath)
-          # unless File.directory?(dirname)
-          #   FileUtils.mkdir_p(dirname)
-          # end
+          #check if there is a link to the course page
+          #courseLinks = tt_doc.css('a[target=details]')
+          courseLink = resp.link_with(:text => course_code)
+          if(!courseLink.nil?) then
+            # courseHref = MTTURL + courseLinks[0]['href']
+            # puts "#{subject_code}-#{course_code} href: #{courseHref.inspect}"
+            # coursePage = a.get(courseHref)
+            coursePage = courseLink.click
 
-          # File.write(fPath, resp.body)
-          # puts 'Wrote: ' + fPath
+            #write the contents of the course page to a file for parsing later
+            fPath = 'courses/' + subject_code + '/' + course_code + '.html'
+            dirname = File.dirname(fPath)
+            unless File.directory?(dirname)
+             FileUtils.mkdir_p(dirname)
+            end
+
+            File.write(fPath, coursePage.body)
+            puts 'Wrote: ' + fPath
+          end
         end
       end
     end
   end
 end
 
-
-# submit login form
-#resp = a.submit(ttForm, ttForm.buttons.first)
-#puts resp.body
+getTimetablePages()
